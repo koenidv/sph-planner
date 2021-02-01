543826f714e13885d8ec4ffd6f5013bda2790ced
koenidv <32238636+koenidv@users.noreply.github.com>
2021-02-01 13:03:15 +0100

    Display EVA on timetable
    Signed-off-by: koenidv <32238636+koenidv@users.noreply.github.com>

--- app/src/main/java/de/koenidv/sph/ui/TimetableViewFragment.kt
+++ app/src/main/java/de/koenidv/sph/ui/TimetableViewFragment.kt
@@ -1,6 +1,7 @@
 package de.koenidv.sph.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +12,17 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import de.koenidv.sph.R
+import de.koenidv.sph.SphPlanner
 import de.koenidv.sph.adapters.LessonsAdapter
+import de.koenidv.sph.database.ChangesDb
 import de.koenidv.sph.database.TimetableDb
+import de.koenidv.sph.objects.Change
 import de.koenidv.sph.objects.TimetableEntry
 import de.koenidv.sph.parsing.Utility
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
 
 
 // Created on 24.12.2020 by koenidv.
@@ -22,6 +30,7 @@ class TimetableViewFragment : Fragment() {
     private var expanded: Boolean = false
     private var viewAll: Boolean = false
     private var openOnClick: Boolean = true
+    private var withChanges: Boolean = true
 
     // List of days of hours of concurrent lessons
     private lateinit var timetable: List<List<List<TimetableEntry>>>
@@ -32,6 +41,7 @@ class TimetableViewFragment : Fragment() {
             expanded = it.getBoolean("expanded")
             viewAll = it.getBoolean("viewAll")
             openOnClick = it.getBoolean("openOnClick")
+            withChanges = it.getBoolean("withChanges")
         }
     }
 
@@ -42,7 +52,9 @@ class TimetableViewFragment : Fragment() {
         val prefs = requireContext().getSharedPreferences("sharedPrefs", AppCompatActivity.MODE_PRIVATE)
 
         // Get favorites if viewAll is false, all lessons otherwise
+        val time1 = Date().time
         timetable = TimetableDb.instance!!.get(!viewAll)
+        Log.d(SphPlanner.TAG, "Time used for timetable mapping: " + (Date().time - time1) + "ms")
         val recyclerViewPool = RecyclerView.RecycledViewPool()
 
         // Get the 5 recyclerviews
@@ -69,11 +81,13 @@ class TimetableViewFragment : Fragment() {
 
         // Set up lessons adapters
         if (!timetable.isNullOrEmpty()) {
+            val time2 = Date().time
             monday.adapter = LessonsAdapter(timetable[0], expanded, viewAll, onClick = onClick)
             tuesday.adapter = LessonsAdapter(timetable[1], expanded, viewAll, onClick = onClick)
             wednesday.adapter = LessonsAdapter(timetable[2], expanded, viewAll, onClick = onClick)
             thursday.adapter = LessonsAdapter(timetable[3], expanded, viewAll, onClick = onClick)
             friday.adapter = LessonsAdapter(timetable[4], expanded, viewAll, onClick = onClick)
+            Log.d(SphPlanner.TAG, "Time used for timetable recycler creation: " + (Date().time - time2) + "ms")
         }
 
         // Highlight today's recycler with the theme color at ~15% opacity
@@ -87,6 +101,51 @@ class TimetableViewFragment : Fragment() {
             2 -> wednesday.background = background
             3 -> thursday.background = background
             else -> friday.background = background
+        }
+
+        if (withChanges) {
+            // Get all changes for favorite courses, sorted by date and lesson
+            val changes = ChangesDb.instance!!.getFavorites()
+            if (changes.isNotEmpty()) {
+                val changesByDay = mutableListOf<List<Change>>()
+                val changesThisDay = mutableListOf<Change>()
+                // Having the list already ordered means we can map it more efficently,
+                // by assuming all entries for one day will be en bloc
+                var lastDate = changes[0].date
+
+                @Suppress("DEPRECATION")
+                var lastDay = lastDate.day - 1 // Monday is first day
+                // Add each change to the corresponding day
+                for (change in changes) {
+                    // If this is the next day, save last day and continue with the next
+                    if (change.date != lastDate) {
+                        changesByDay.add(lastDay, changesThisDay.toList())
+                        changesThisDay.clear()
+                        // Update day
+                        lastDate = change.date
+                        @Suppress("DEPRECATION")
+                        lastDay = lastDate.day - 1
+                    }
+                    changesThisDay.add(change)
+                }
+                // Add the last day to changesByDay list
+                changesByDay.add(lastDay, changesThisDay.toList())
+                changesThisDay.clear()
+
+                // Apply changes to the RecyclerViews
+                GlobalScope.launch {
+                    // Wait 200ms to avoid blocking the ui thread
+                    // Will display regular lesson first and then switch to updated lesson
+                    delay(200)
+                    if (activity != null)
+                        requireActivity().runOnUiThread {
+                            if (changesByDay.getOrNull(0) != null)
+                                (monday.adapter as LessonsAdapter).applyChanges(changesByDay[0])
+                        }
+                }
+
+            }
+
         }
 
         return view
@@ -114,9 +173,11 @@ class TimetableViewFragment : Fragment() {
                 parentFragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment,
                                 instantiate(requireContext(), TimetableFragment().javaClass.name,
-                                        bundleOf("expanded" to this.expanded,
+                                        bundleOf(
+                                                "expanded" to this.expanded,
                                                 "viewAll" to this.viewAll,
-                                                "openOnClick" to this.openOnClick)))
+                                                "openOnClick" to this.openOnClick,
+                                                "withChanges" to this.withChanges)))
                         .commit()
             } else {
                 // Update timetable
