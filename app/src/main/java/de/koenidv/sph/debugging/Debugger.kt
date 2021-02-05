49f4db5f5a6604d16138a4e5a46c7666df8cb84a
koenidv <32238636+koenidv@users.noreply.github.com>
2021-02-05 20:36:35 +0100

    Upload logs to dogbin and share
    Signed-off-by: koenidv <32238636+koenidv@users.noreply.github.com>

--- app/src/main/java/de/koenidv/sph/debugging/Debugger.kt
+++ app/src/main/java/de/koenidv/sph/debugging/Debugger.kt
@@ -1,5 +1,15 @@
 package de.koenidv.sph.debugging
 
+import android.content.Intent
+import android.widget.Toast
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.google.gson.GsonBuilder
+import de.koenidv.sph.R
+import de.koenidv.sph.SphPlanner
+import org.json.JSONObject
+
 //  Created by koenidv on 05.02.2021.
 object Debugger {
     const val DEBUGGING_ENABLED = true
@@ -12,6 +22,9 @@ object Debugger {
 
     private val logs = mutableListOf<DebugLog>()
 
+    /**
+     * Adds the DebugLog to the list of logs
+     */
     fun log(log: DebugLog) {
         logs.add(log)
     }
@@ -27,6 +40,55 @@ object Debugger {
         } catch (e: Exception) {
             "Reponse String does not contain a title"
         }
+    }
+
+    /**
+     * Uploads the
+     */
+    fun share() {
+        upload {
+            // Share link to to dogbin
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, it)
+                this.type = "text/plain"
+            }
+            val chooser = Intent.createChooser(sendIntent,
+                    SphPlanner.applicationContext().getString(R.string.debugger_share))
+                    .apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    }
+            SphPlanner.applicationContext().startActivity(chooser)
+        }
+    }
+
+    /**
+     * Uploads all logged entries and calls back with a link to it
+     */
+    private fun upload(callback: (url: String) -> Unit) {
+        // Upload text to https://del.dog, API key in BuildConfig
+        // If you need the apikeys.properties file, contact koenidv
+        AndroidNetworking.post("https://del.dog/documents")
+                // Add pretty-printed json object string as body
+                .addStringBody(GsonBuilder().setPrettyPrinting().create().toJson(logs))
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+                    override fun onResponse(response: JSONObject) {
+                        try {
+                            callback("https://del.dog/" + response.get("key"))
+                        } catch (e: Exception) {
+                            Toast.makeText(SphPlanner.applicationContext(),
+                                    "Uploading to dogbin failed: " + e.message,
+                                    Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        Toast.makeText(SphPlanner.applicationContext(),
+                                "Uploading to dogbin failed", Toast.LENGTH_LONG).show()
+                    }
+
+                })
     }
 
 
