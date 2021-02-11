fa91079aa8d653a3e358787a6a524b3a4d3535c1
LbTobi <62104345+LbTobi@users.noreply.github.com>
2021-02-11 22:20:34 +0100

    implement correct holiday names

--- app/src/main/java/de/koenidv/sph/adapters/HolidaysAdapter.kt
+++ app/src/main/java/de/koenidv/sph/adapters/HolidaysAdapter.kt
@@ -1,15 +1,21 @@
 package de.koenidv.sph.adapters
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.system.Os.bind
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import de.koenidv.sph.objects.Holiday
 import de.koenidv.sph.R
+import de.koenidv.sph.SphPlanner
+import de.koenidv.sph.SphPlanner.Companion.applicationContext
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
 
 
 //  Created by LbTobi on 10.02.2021
@@ -28,7 +34,25 @@ class HolidaysAdapter(private val holidays: List<Holiday>) :
         currentHoliday = holiday
 
         //place data in current holiday item
-        name.text = holiday.name
+
+        //name
+        val currentName = currentHoliday.name
+
+        name.text = when (currentName) {
+            "osterferien" -> applicationContext().getString(R.string.holidays_spring)
+            "sommerferien" -> applicationContext().getString(R.string.holidays_summer)
+            "herbstferien" -> applicationContext().getString(R.string.holidays_autumn)
+            "weihnachtsferien" -> applicationContext().getString(R.string.holidays_winter)
+            else -> currentName
+        }
+
+        //date
+        val formatter = SimpleDateFormat.getDateInstance()
+        val startDate = formatter.format(currentHoliday.start)
+
+
+       //date.text = startDate
+
 
 
 
