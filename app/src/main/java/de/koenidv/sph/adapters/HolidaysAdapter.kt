a41b0578dd448fcd7063315e04c5ded3ed13849f
LbTobi <62104345+lbtobi@users.noreply.github.com>
2021-02-12 23:35:03 +0100

    holidays Fragment working

--- app/src/main/java/de/koenidv/sph/adapters/HolidaysAdapter.kt
+++ app/src/main/java/de/koenidv/sph/adapters/HolidaysAdapter.kt
@@ -1,65 +1,87 @@
 package de.koenidv.sph.adapters
 
-import android.annotation.SuppressLint
-import android.os.Build
-import android.system.Os.bind
+
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import de.koenidv.sph.objects.Holiday
 import de.koenidv.sph.R
-import de.koenidv.sph.SphPlanner
 import de.koenidv.sph.SphPlanner.Companion.applicationContext
+import de.koenidv.sph.debugging.Debugger.prefs
+import de.koenidv.sph.parsing.Utility
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 //  Created by LbTobi on 10.02.2021
 class HolidaysAdapter(private val holidays: List<Holiday>) :
     RecyclerView.Adapter<HolidaysAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val themeColor = prefs.getInt("themeColor", 0)
 
-    private val name = view.findViewById<TextView>(R.id.holidayDateTextView)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val layout = view.findViewById<ConstraintLayout>(R.id.holidayLayout)
+    private val name = view.findViewById<TextView>(R.id.holidayNameTextView)
     private val date = view.findViewById<TextView>(R.id.holidayDateTextView)
     private val remaining = view.findViewById<TextView>(R.id.holidayRemainingTextView)
     lateinit var currentHoliday: Holiday
 
 
-    fun bind(holiday: Holiday) {
+    fun bind(holiday: Holiday, position: Int) {
         currentHoliday = holiday
 
         //place data in current holiday item
 
         //name
         val currentName = currentHoliday.name
+        val year = currentHoliday.year
 
         name.text = when (currentName) {
-            "osterferien" -> applicationContext().getString(R.string.holidays_spring)
-            "sommerferien" -> applicationContext().getString(R.string.holidays_summer)
-            "herbstferien" -> applicationContext().getString(R.string.holidays_autumn)
-            "weihnachtsferien" -> applicationContext().getString(R.string.holidays_winter)
+            "osterferien" -> applicationContext().getString(R.string.holidays_spring) + " " + year
+            "sommerferien" -> applicationContext().getString(R.string.holidays_summer) + " $year"
+            "herbstferien" -> applicationContext().getString(R.string.holidays_autumn) + " $year"
+            "weihnachtsferien" -> applicationContext().getString(R.string.holidays_winter) + " $year"
             else -> currentName
         }
 
-        //date
+        //date stuff
         val formatter = SimpleDateFormat.getDateInstance()
         val startDate = formatter.format(currentHoliday.start)
+        val endDate = formatter.format(currentHoliday.end)
+        val currentDate = Date().time
+        val remainingTime = currentHoliday.start.time - currentDate
+        val remainingDays = remainingTime / 86400000
 
+        date.text = applicationContext().getString(R.string.holidays_date)
+                .replace("%s", startDate)
+                .replace("%e", endDate)
+        // TODO: 12/02/2021 don't display year in date Textfield
 
-       //date.text = startDate
+        //remaining
+        if (remainingDays >= 7) {
+            remaining.text = applicationContext().getString(R.string.holidays_remaining_weeks)
+                    .replace("%w", (remainingDays / 7).toString())
+                    .replace("%d", (remainingDays - (remainingDays / 7) * 7).toString())
+        } else {
+            remaining.text = applicationContext().getString(R.string.holidays_remaining_days, remainingDays)
+        }
 
-
-
-
-        // TODO: 10/02/2021 implement date stuff
-
-
-
+        // Tint background with holiday color at 15% for next 4 Holidays
+        if (position < 4) {
+            val color = when (currentName) {
+                "osterferien" -> applicationContext().getColor(R.color.holiday_color_spring)
+                "sommerferien" -> applicationContext().getColor(R.color.holiday_color_summer)
+                "herbstferien" -> applicationContext().getColor(R.color.holiday_color_autumn)
+                "weihnachtsferien" -> applicationContext().getColor(R.color.holiday_color_winter)
+                else -> Color.TRANSPARENT
+            }
+            Utility.tintBackground(layout, color, 0x50000000)
+        } else
+            layout.background.clearColorFilter()
     }
 
 }
@@ -71,7 +93,7 @@ class HolidaysAdapter(private val holidays: List<Holiday>) :
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-        viewHolder.bind(holidays[position])
+        viewHolder.bind(holidays[position], position)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/de/koenidv/sph/ui/HolidaysFragment.kt
+++ app/src/main/java/de/koenidv/sph/ui/HolidaysFragment.kt
@@ -9,29 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import de.koenidv.sph.R
 import de.koenidv.sph.adapters.HolidaysAdapter
 import de.koenidv.sph.database.HolidaysDb
-/*
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-*/
-/**
- * A simple [Fragment] subclass.
- * Use the [HolidaysFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HolidaysFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-  /*  private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }*/
+class HolidaysFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -45,24 +24,4 @@ class HolidaysFragment : Fragment() {
 
         return view
     }
-
-  /*  companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HolidaysFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                HolidaysFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }   */
 }
