70530105131fbd312714ac19c923412b0ec58c62
Florian König <koenidv@gmail.com>
2023-11-16 00:03:15 +0100

    demo: add changes demo data

--- app/src/main/java/de/koenidv/sph/database/demodata/Demodata.kt
+++ app/src/main/java/de/koenidv/sph/database/demodata/Demodata.kt
@@ -3,6 +3,7 @@ package de.koenidv.sph.database.demodata
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Color
 import de.koenidv.sph.database.AttachmentsDb
+import de.koenidv.sph.database.ChangesDb
 import de.koenidv.sph.database.CoursesDb
 import de.koenidv.sph.database.DatabaseHelper
 import de.koenidv.sph.database.FileAttachmentsDb
@@ -14,6 +15,7 @@ import de.koenidv.sph.database.TasksDb
 import de.koenidv.sph.database.TimetableDb
 import de.koenidv.sph.database.UsersDb
 import de.koenidv.sph.networking.Holidays
+import de.koenidv.sph.objects.Change
 import de.koenidv.sph.objects.Course
 import de.koenidv.sph.objects.FileAttachment
 import de.koenidv.sph.objects.FunctionTile
@@ -68,8 +70,8 @@ class Demodata {
             Course(courseId = "d01", fullname = "Sachen machen", id_teacher = "teacher1", color = Color.parseColor("#FFA726"), isFavorite = true),
             Course(courseId = "d02", fullname = "Dingsen & Döngsen", id_teacher = "teacher2", color = Color.parseColor("#4CAF50"), isFavorite = true),
             Course(courseId = "cs101", fullname =  "Introduction to Computer Science", id_teacher = "teacher1", color =  Color.parseColor("#9C27B0"), isFavorite = true),
-            Course(courseId = "webdev", fullname =  "Web Development", id_teacher = "teacher2", color =  Color.parseColor("#2196F3"), isFavorite =  true),
-            Course(courseId = "dbms", fullname =  "Database Management Systems", id_teacher = "taecher3", color =  Color.parseColor("#8BC34A"), isFavorite = true),
+            Course(courseId = "webdev01", fullname =  "Web Development", id_teacher = "teacher2", color =  Color.parseColor("#2196F3"), isFavorite =  true),
+            Course(courseId = "dbms01", fullname =  "Database Management Systems", id_teacher = "teacher3", color =  Color.parseColor("#8BC34A"), isFavorite = true),
             Course(courseId = "ai2023", fullname =  "Artificial Intelligence", id_teacher = "teacher12", color =  Color.parseColor("#FF5722"), isFavorite =  true),
         ))
 
@@ -77,19 +79,24 @@ class Demodata {
             Lesson(idCourse = "se21", day = 0, hour = 1, room = "Jungle"),
             Lesson(idCourse = "se21", day = 0, hour = 2, room = "Jungle"),
             Lesson(idCourse = "d01", day = 0, hour = 3, room = "Roomy"),
-            Lesson(idCourse = "dbms", day = 0, hour = 4, room = "R2"),
+            Lesson(idCourse = "dbms01", day = 0, hour = 4, room = "R2"),
             Lesson(idCourse = "cs101", day = 0, hour = 5, room = "Matrix"),
-            Lesson(idCourse = "webdev", day = 0, hour = 6, room = "Matrix"),
+            Lesson(idCourse = "webdev01", day = 0, hour = 6, room = "Matrix"),
             Lesson(idCourse = "ai2023", day = 1, hour = 3, room = "Matrix"),
             Lesson(idCourse = "ai2023", day = 1, hour = 4, room = "Matrix"),
             Lesson(idCourse = "ai2023", day = 1, hour = 5, room = "Matrix"),
             Lesson(idCourse = "d02", day = 1, hour = 6, room = "Matrix"),
             Lesson(idCourse = "se21", day = 2, hour = 2, room = "Jungle"),
-            Lesson(idCourse = "webdev", day = 2, hour = 4, room = "Jungle"),
-            Lesson(idCourse = "dbms", day = 2, hour = 5, room = "R2"),
-            Lesson(idCourse = "dbms", day = 2, hour = 6, room = "R2"),
+            Lesson(idCourse = "webdev01", day = 2, hour = 4, room = "Jungle"),
+            Lesson(idCourse = "dbms01", day = 2, hour = 5, room = "R2"),
+            Lesson(idCourse = "dbms01", day = 2, hour = 6, room = "R2"),
             Lesson(idCourse = "d01", day = 2, hour = 8, room = "Team HQ"),
             Lesson(idCourse = "d01", day = 2, hour = 9, room = "Team HQ"),
+        ))
+
+        ChangesDb.instance!!.save(listOf(
+            Change(id_course = "dbms01", date = LocalDate.of(2023, 11, 15).toDate(), lessons = listOf(5, 6), type = Change.TYPE_CANCELLED),
+            Change(id_course = "d01", date = LocalDate.of(2023, 11, 15).toDate(), lessons = listOf(8, 9), type = Change.TYPE_EXAM)
         ))
 
         FunctionTilesDb.getInstance().save(listOf(
@@ -118,9 +125,9 @@ class Demodata {
             Post(postId = "d01_post_3", id_course = "d01", date = LocalDate.of(2023, 10, 9).toDate(), title = "Materials and Resources", description = "Find out where to access materials and resources for your 'Sachen machen' projects. We've got you covered!", unread = true),
             Post(postId = "cs101_post_1", id_course = "cs101", date = LocalDate.of(2023, 10, 7).toDate(), title = "Welcome to 'Introduction to Computer Science'!", description = "Get ready to explore the world of computer science and coding. We'll start with the basics.", unread = true),
             Post(postId = "cs101_post_2", id_course = "cs101", date = LocalDate.of(2023, 10, 8).toDate(), title = "Understanding Algorithms and Data Structures", description = "Learn about the fundamental concepts of algorithms and data structures that power computing.", unread = true),
-            Post(postId = "webdev_post_1", id_course = "webdev", date = LocalDate.of(2023, 10, 8).toDate(), title = "Welcome to 'Web Development'!", description = "Discover the exciting world of web development and the technologies we'll be working with in this course.", unread = true),
-            Post(postId = "webdev_post_2", id_course = "webdev", date = LocalDate.of(2023, 10, 9).toDate(), title = "Creating Your First Web Page", description = "Let's dive right in and create your very first web page using HTML and CSS.", unread = true),
-            Post(postId = "dbms_post_1", id_course = "dbms", date = LocalDate.of(2023, 10, 9).toDate(), title = "Welcome to 'Database Management Systems'!", description = "Explore the world of databases, from relational databases to NoSQL solutions, in this course.", unread = true),
+            Post(postId = "webdev01_post_1", id_course = "webdev01", date = LocalDate.of(2023, 10, 8).toDate(), title = "Welcome to 'Web Development'!", description = "Discover the exciting world of web development and the technologies we'll be working with in this course.", unread = true),
+            Post(postId = "webdev01_post_2", id_course = "webdev01", date = LocalDate.of(2023, 10, 9).toDate(), title = "Creating Your First Web Page", description = "Let's dive right in and create your very first web page using HTML and CSS.", unread = true),
+            Post(postId = "dbms01_post_1", id_course = "dbms01", date = LocalDate.of(2023, 10, 9).toDate(), title = "Welcome to 'Database Management Systems'!", description = "Explore the world of databases, from relational databases to NoSQL solutions, in this course.", unread = true),
             Post(postId = "ai2023_post_1", id_course = "ai2023", date = LocalDate.of(2023, 10, 10).toDate(), title = "Welcome to 'Artificial Intelligence'!", description = "Dive into the fascinating field of artificial intelligence and its real-world applications.", unread = true),
             Post(postId = "ai2023_post_2", id_course = "ai2023", date = LocalDate.of(2023, 10, 11).toDate(), title = "Machine Learning Basics", description = "Get started with machine learning and understand its core concepts in this post.", unread = true)
         ))
@@ -132,19 +139,19 @@ class Demodata {
             Task(taskId = "se21_task_5", id_course = "se21", id_post = "se21_post_5", description = "Set Up Local Data Storage", date = LocalDate.of(2023, 10, 10).toDate(), isDone = false),
             Task(taskId = "cs101_task_1", id_course = "cs101", id_post = "cs101_post_1", description = "Install Python and IDE", date = LocalDate.of(2023, 10, 7).toDate(), isDone = false),
             Task(taskId = "cs101_task_2", id_course = "cs101", id_post = "cs101_post_2", description = "Implement Sorting Algorithms", date = LocalDate.of(2023, 10, 8).toDate(), isDone = false),
-            Task(taskId = "webdev_task_1", id_course = "webdev", id_post = "webdev_post_1", description = "Set Up Development Environment", date = LocalDate.of(2023, 10, 8).toDate(), isDone = false),
-            Task(taskId = "webdev_task_2", id_course = "webdev", id_post = "webdev_post_2", description = "Create a Simple Web Page", date = LocalDate.of(2023, 10, 9).toDate(), isDone = false),
+            Task(taskId = "webdev01_task_1", id_course = "webdev01", id_post = "webdev01_post_1", description = "Set Up Development Environment", date = LocalDate.of(2023, 10, 8).toDate(), isDone = false),
+            Task(taskId = "webdev01_task_2", id_course = "webdev01", id_post = "webdev01_post_2", description = "Create a Simple Web Page", date = LocalDate.of(2023, 10, 9).toDate(), isDone = false),
             Task(taskId = "ai2023_task_1", id_course = "ai2023", id_post = "ai2023_post_1", description = "Explore AI Applications", date = LocalDate.of(2023, 10, 10).toDate(), isDone = false),
             Task(taskId = "ai2023_task_2", id_course = "ai2023", id_post = "ai2023_post_2", description = "Implement Linear Regression", date = LocalDate.of(2023, 10, 11).toDate(), isDone = false),
             Task(taskId = "d01_task_1", id_course = "d01", id_post = "d01_post_1", description = "Choose a Project Idea", date = LocalDate.of(2023, 10, 7).toDate(), isDone = false),
             Task(taskId = "d01_task_2", id_course = "d01", id_post = "d01_post_1", description = "Gather Necessary Materials", date = LocalDate.of(2023, 10, 7).toDate(), isDone = false),
             Task(taskId = "d01_task_3", id_course = "d01", id_post = "d01_post_2", description = "Participate in Brainstorming", date = LocalDate.of(2023, 10, 8).toDate(), isDone = false),
-            Task(taskId = "dbms_task_1", id_course = "dbms", id_post = "dbms_post_1", description = "Explore Relational Databases", date = LocalDate.of(2023, 10, 9).toDate(), isDone = false)
+            Task(taskId = "dbms01_task_1", id_course = "dbms01", id_post = "dbms01_post_1", description = "Explore Relational Databases", date = LocalDate.of(2023, 10, 9).toDate(), isDone = false)
         ))
 
         FileAttachmentsDb.getInstance().save(listOf(
             FileAttachment("ai2023_post_2_attach_1", "ai2023", "ai2023_post_2", "Shanghai at Night", LocalDate.of(2023, 10, 11).toDate(), "https://images.unsplash.com/photo-1696142990758-581061f2801d", "4 MB", "jpeg", true, null),
-            FileAttachment("webdev_post_2_attach_1", "webdev", "webdev_post_2", "SE_22 Self-Assessment", LocalDate.of(2023, 10, 8).toDate(), "https://docs.google.com/document/d/1qhwxb_HSE4oBKiGDgJu2XCnzSICIjYNvwUSaF8qA8Ug/export?format=pdf", "44 KB", "pdf", true, null),
+            FileAttachment("webdev01_post_2_attach_1", "webdev01", "webdev01_post_2", "SE_22 Self-Assessment", LocalDate.of(2023, 10, 8).toDate(), "https://docs.google.com/document/d/1qhwxb_HSE4oBKiGDgJu2XCnzSICIjYNvwUSaF8qA8Ug/export?format=pdf", "44 KB", "pdf", true, null),
         ))
 
         LinkAttachmentsDb.getInstance().save(listOf(
